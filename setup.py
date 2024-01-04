123e3a579cdf15df01bf934537f38362aa38470f
jmpr1 <jmpr1991@gmail.com>
2024-01-04 11:17:40 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='JMP_QC_ALGO',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Library with Quantum algorithms',
+    author='Juan Manuel Pavón Rodríguez',
+)
