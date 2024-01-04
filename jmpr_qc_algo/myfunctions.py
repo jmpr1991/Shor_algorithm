a348163a24803a1bf3de835355d6d26ede663390
jmpr1 <jmpr1991@gmail.com>
2024-01-04 16:31:03 +0100

    QFT function created

--- jmpr_qc_algo/myfunctions.py
+++ jmpr_qc_algo/myfunctions.py
@@ -1,5 +1,9 @@
 from math import radians, cos, sin, asin, sqrt
-import qiskit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
+from qiskit.tools.visualization import plot_histogram, plot_bloch_multivector
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
     """
@@ -24,5 +28,33 @@ def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
     r = 6371 # Radius of earth in kilometers
     return c * r
 
-def qft_jmp(lon1: float, lat1: float, lon2: float, lat2: float):
-    "Calculate the Quantum Fourier Transform"
+def qft_jmp(q_circuit: classmethod, num_qubits: int)->classmethod:
+    """
+    Calculate the Quantum Fourier Transform of the input circuit
+    and returns the circuit
+    :param q_circuit: input circuit
+    :param num_qubits: number of qubits of the QFT circuit
+    :return: circuit with the QFT added
+    """
+    circuit = QuantumCircuit(num_qubits)
+
+    for qubit_i in reversed(range(num_qubits)):
+        # implement Hadamard gate
+        circuit.h(qubit_i)
+
+        # implement control phase gates
+        for qubit_j in (range(qubit_i)):
+            phase = 2*np.pi/(2**(qubit_i - qubit_j + 1))
+            circuit.cp(phase, qubit_j, qubit_i)
+        circuit.barrier()
+
+    # swap qubits
+    n_qubits = np.floor(num_qubits/2)
+    for qubit_i in range(int(n_qubits)):
+        circuit.swap(qubit_i, num_qubits-1-qubit_i)
+
+    circuit = q_circuit.compose(circuit, range(num_qubits))
+    return circuit
+
+
+
--- test/test_myfunctions.py
+++ test/test_myfunctions.py
@@ -1,7 +1,38 @@
 from jmpr_qc_algo import myfunctions
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
+
+import numpy as np
 
 def test_haversine():
     # Amsterdam to Berlin
     assert myfunctions.haversine(
         4.895168, 52.370216, 13.404954, 52.520008
     ) == 576.6625818456291
+
+def test_qft_jmp():
+    # 3 qubits QFT
+    qr = QuantumRegister(3)
+    cr = ClassicalRegister(3)
+    circuit = QuantumCircuit(qr, cr)
+
+    #test case
+    circuit.x([0, 2])
+
+    circuit = myfunctions.qft_jmp(circuit, 3)
+    # circuit_2.measure(qr,cr)
+    print(circuit)
+
+    simulator = Aer.get_backend('statevector_simulator')
+
+    statevector = simulator.run(circuit).result().get_statevector()
+    statevecto_test = [3.53553391e-01-8.65956056e-17j,
+             -2.50000000e-01-2.50000000e-01j,
+              1.08244507e-16+3.53553391e-01j,
+              2.50000000e-01-2.50000000e-01j,
+             -3.53553391e-01+8.65956056e-17j,
+              2.50000000e-01+2.50000000e-01j,
+             -1.08244507e-16-3.53553391e-01j,
+             -2.50000000e-01+2.50000000e-01j]
+    for i in range(len(statevecto_test)):
+        assert np.round(statevector[i], 4) == np.round(statevecto_test[i], 4)
+
