1c63bf855f286cb53999c39fe961df98e5b03c32
jmpr1 <jmpr1991@gmail.com>
2024-01-09 20:11:48 +0100

    QFT function updated with the possibility to inject a new input (list_qubits)

--- jmpr_qc_algo/myfunctions.py
+++ jmpr_qc_algo/myfunctions.py
@@ -28,33 +28,59 @@ def haversine(lon1: float, lat1: float, lon2: float, lat2: float) -> float:
     r = 6371 # Radius of earth in kilometers
     return c * r
 
-def qft_jmp(q_circuit: classmethod, num_qubits: int)->classmethod:
+def qft_jmp(q_circuit: 'classmethod', list_qubits: 'int' = None) -> 'classmethod':
     """
     Calculate the Quantum Fourier Transform of the input circuit
     and returns the circuit
     :param q_circuit: input circuit
-    :param num_qubits: number of qubits of the QFT circuit
+    :param list_qubits: [optional parameter] number of qubits of the QFT circuit
     :return: circuit with the QFT added
     """
-    circuit = QuantumCircuit(num_qubits)
 
+    # Create the circuit for the QFT depending on the list_qubit input
+    if list_qubits==None:
+        num_qubits = q_circuit.num_qubits
+        qft_circuit = QuantumCircuit(num_qubits)
+    else:
+        num_qubits = len(list_qubits)
+        qft_circuit = QuantumCircuit(num_qubits)
+
+    # Build the QFT (Nielsen & Chuang section 5.1)
     for qubit_i in reversed(range(num_qubits)):
         # implement Hadamard gate
-        circuit.h(qubit_i)
+        qft_circuit.h(qubit_i)
 
         # implement control phase gates
         for qubit_j in (range(qubit_i)):
             phase = 2*np.pi/(2**(qubit_i - qubit_j + 1))
-            circuit.cp(phase, qubit_j, qubit_i)
-        circuit.barrier()
+            qft_circuit.cp(phase, qubit_j, qubit_i)
+        qft_circuit.barrier()
 
     # swap qubits
     n_qubits = np.floor(num_qubits/2)
     for qubit_i in range(int(n_qubits)):
-        circuit.swap(qubit_i, num_qubits-1-qubit_i)
+        qft_circuit.swap(qubit_i, num_qubits-1-qubit_i)
 
-    circuit = q_circuit.compose(circuit, range(num_qubits))
+    # Compose the final circuit
+    if list_qubits==None:
+        circuit = q_circuit.compose(qft_circuit, range(num_qubits))
+    else:
+        circuit = q_circuit.compose(qft_circuit, list_qubits)
+
     return circuit
 
+qubit_list = [0, 1, 2]
+qr = QuantumRegister(4)
+cr = ClassicalRegister(len(qubit_list))
+circuit = QuantumCircuit(qr, cr)
 
+# test case
+circuit.x([0, 2])
 
+circuit = qft_jmp(circuit, qubit_list)
+print(circuit)
+
+simulator = Aer.get_backend('statevector_simulator')
+
+statevector = simulator.run(circuit).result().get_statevector()
+print(statevector)
--- test/test_myfunctions.py
+++ test/test_myfunctions.py
@@ -2,15 +2,11 @@ from jmpr_qc_algo import myfunctions
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
 
 import numpy as np
-
-def test_haversine():
-    # Amsterdam to Berlin
-    assert myfunctions.haversine(
-        4.895168, 52.370216, 13.404954, 52.520008
-    ) == 576.6625818456291
-
-def test_qft_jmp():
-    # 3 qubits QFT
+def test_qft_jmp_1():
+    """
+    This test check that the qft_jmp function build a QFT circuit on top of the input circuit. No list of qubits is
+    is provided in this test
+    """
     qr = QuantumRegister(3)
     cr = ClassicalRegister(3)
     circuit = QuantumCircuit(qr, cr)
@@ -18,9 +14,7 @@ def test_qft_jmp():
     #test case
     circuit.x([0, 2])
 
-    circuit = myfunctions.qft_jmp(circuit, 3)
-    # circuit_2.measure(qr,cr)
-    print(circuit)
+    circuit = myfunctions.qft_jmp(circuit)
 
     simulator = Aer.get_backend('statevector_simulator')
 
@@ -36,3 +30,39 @@ def test_qft_jmp():
     for i in range(len(statevecto_test)):
         assert np.round(statevector[i], 4) == np.round(statevecto_test[i], 4)
 
+def test_qft_jmp_2():
+    """
+       This test check that the qft_jmp function build a QFT circuit on top of the input circuit. A list of Qubits is
+       provided in this test
+       """
+    qubit_list = [0, 1, 2]
+    qr = QuantumRegister(4)
+    cr = ClassicalRegister(len(qubit_list))
+    circuit = QuantumCircuit(qr, cr)
+
+    # test case
+    circuit.x([0, 2])
+
+    circuit = myfunctions.qft_jmp(circuit, qubit_list)
+
+    simulator = Aer.get_backend('statevector_simulator')
+
+    statevector = simulator.run(circuit).result().get_statevector()
+    statevecto_test = [3.53553391e-01-8.65956056e-17j,
+             -2.50000000e-01-2.50000000e-01j,
+              1.08244507e-16+3.53553391e-01j,
+              2.50000000e-01-2.50000000e-01j,
+             -3.53553391e-01+8.65956056e-17j,
+              2.50000000e-01+2.50000000e-01j,
+             -1.08244507e-16-3.53553391e-01j,
+             -2.50000000e-01+2.50000000e-01j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j,
+              0.00000000e+00+0.00000000e+00j]
+    for i in range(len(statevecto_test)):
+        assert np.round(statevector[i], 4) == np.round(statevecto_test[i], 4)
