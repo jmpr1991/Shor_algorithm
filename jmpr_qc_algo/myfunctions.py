e1b39743f3af48e57c8cc62f69dcf39822f329b4
jmpr1 <jmpr1991@gmail.com>
2024-01-19 22:49:03 +0100

    modular exponentiation on going

--- jmpr_qc_algo/myfunctions.py
+++ jmpr_qc_algo/myfunctions.py
@@ -1,4 +1,5 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, Aer
+from qiskit.quantum_info.operators import Operator
 from qiskit.tools.visualization import plot_histogram, plot_bloch_multivector
 from qiskit.circuit.library import QFT
 
@@ -102,7 +103,7 @@ def phase_estimation_operator_jmp(estimator_circuit: 'classmethod', phi_circuit:
 
     # Phase estimation procedure
     step = 0
-    for control_qubit in range(estimator_register_size):
+    for control_qubit in range(estimator_qubits):
 
         for exponent in range(2**step):
             circuit.cp(2*np.pi * phase, estimator_register[control_qubit], phi_register)
@@ -117,3 +118,64 @@ def phase_estimation_operator_jmp(estimator_circuit: 'classmethod', phi_circuit:
     phase_estimator_circuit.measure(estimator_register, classical_register)
 
     return phase_estimator_circuit
+
+
+def modular_exponentiation_jmp(number_to_factor: 'int', number_module: 'int') -> 'list':
+    """
+    This function computes the list of operator of the modular exponentiation to be used in Shor algorithm
+    :param number_to_factor: number which is going to be factorized
+    :param number_module: module number to be used in the computations
+    :return operators: list of operators following the modular exponentiation
+    """
+
+    # compute the number of bits of the number to factor
+    length = len(bin(number_to_factor)) - 2
+
+    # initialize state parameter
+    state = []
+    state_binary = []
+    initial_state = 1
+    state.append(initial_state)
+    state_binary.append(bin(initial_state))
+
+    iter = 1
+    while state[iter-1] != 1 or iter == 1:
+        exponentiate = (number_to_factor * state[iter-1]) % number_module
+        state.append(exponentiate)
+        state_binary.append(bin(state[iter]))
+
+        iter += 1
+    # eliminate the last element of the list. It is redundant (state[last]=1)
+    state.pop()
+    state_binary.pop()
+
+    # calculate the period
+    period = len(state)
+
+    circuit_size = int(np.ceil(np.log2(number_module)))
+
+    # create the equivalent circuit and operators
+    circuits = []
+    operators = []
+    for iter in range(period):
+        circuit_iter = QuantumCircuit(circuit_size)
+
+        qubit = 0
+        for iter_binary in reversed(range(len(state_binary[iter]))):
+
+            if state_binary[iter][iter_binary] == '1':
+                circuit_iter.x(qubit)
+            elif state_binary[iter_binary] == 'b':
+                break
+
+            circuits.append(circuit_iter)
+            qubit += 1
+
+        operators.append(Operator(circuits[iter]))
+
+    return operators, circuits
+
+
+operators, circuits = modular_exponentiation_jmp(3, 35)
+print(type(operators))
+print(circuits[0])
